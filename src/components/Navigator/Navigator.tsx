31741b46134e52b9cec10e630d8633e937fb3c73
srgsuh <srg.suhoverhov@gmail.com>
2025-06-28 15:49:00 +0300

    ColorModeButton add to nav panel

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,9 +1,11 @@
 import {Image, Stack} from "@chakra-ui/react";
+import {ColorModeButton} from "../ui/color-mode.tsx";
 
 const Navigator = () => {
     return (
         <Stack bg={{base: "white", _dark: "black"}} direction={"row"} justify={"space-between"}>
             <Image height={"4em"} src={"src/assets/image.png"} alt={"logo"} title={"logo"}></Image>
+            <ColorModeButton height={"4em"}></ColorModeButton>
         </Stack>
     );
 };
