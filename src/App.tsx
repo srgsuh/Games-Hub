53be3583ef6894dfbb798c782eab891d359042ab
srgsuh <srg.suhoverhov@gmail.com>
2025-06-28 14:41:50 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Grid, GridItem} from "@chakra-ui/react";
+
+function App() {
+  return (
+      <>
+          <Grid templateAreas= {{md: '"nav nav" "side main"', base: '"nav" "main"'}}>
+              <GridItem bg={"blue.300"} area="nav">NAVIGATOR</GridItem>
+              <GridItem hideBelow={"md"} bg={"green.300"} area={"side"}>SIDE PAnel</GridItem>
+              <GridItem bg={"violet"} area={"main"}>Main</GridItem>
+          </Grid>
+      </>
+  )
+}
+
+export default App
