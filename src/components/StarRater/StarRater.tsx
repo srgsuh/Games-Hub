73353cd87373bb6dbee4405d373d203f89876377
srgsuh <srg.suhoverhov@gmail.com>
2025-07-06 11:43:17 +0300

    added memoization of the star counting function in StarRater component

--- src/components/StarRater/StarRater.tsx
+++ src/components/StarRater/StarRater.tsx
@@ -1,8 +1,16 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar as solidStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons';
 import { faStar as regularStar } from '@fortawesome/free-regular-svg-icons';
-import type{ReactElement, FC} from 'react';
+import { useMemo } from "react";
+import type{ReactElement, FC} from "react";
 import {HStack, Text} from "@chakra-ui/react";
+
+const STARS={
+    solidStar: (key: number) => <FontAwesomeIcon icon={solidStar} key={key}/>,
+    halfStar: (key: number) => <FontAwesomeIcon icon={faStarHalfStroke} key={key}/>,
+    emptyStar: (key: number) => <FontAwesomeIcon icon={regularStar} key={key}/>,
+};
+
 
 type StarRaterProps = {
     starsCount?: number,
@@ -10,40 +18,42 @@ type StarRaterProps = {
     rating: number,
 };
 
-const StarElements={
-    solidStar: (key: number) => <FontAwesomeIcon icon={solidStar} key={key}/>,
-    halfStar: (key: number) => <FontAwesomeIcon icon={faStarHalfStroke} key={key}/>,
-    emptyStar: (key: number) => <FontAwesomeIcon icon={regularStar} key={key}/>,
+type StarRaterData = {
+    fullStars: number,
+    halfStars: number,
+    starsCount: number,
 };
 
-function getStarsCount(rating: number, maxRating: number, starsCount: number): [number, number] {
+function getStarsCount(rating: number, maxRating: number, starsCount: number)
+    : StarRaterData {
     const normalizedRating = (rating * starsCount) / maxRating;
     const fullStarsMin = Math.floor(normalizedRating);
     const residue = normalizedRating - fullStarsMin;
     const fullStars = fullStarsMin + ((residue >= 0.75) ? 1 : 0);
     const halfStars = ((residue >=  0.25) && (residue < 0.75)? 1: 0);
-    return [fullStars, halfStars];
+    return {fullStars, halfStars, starsCount};
 }
 
-function getStarsArray(rating: number, maxRating: number, starsCount: number): ReactElement[] {
-    const [fullStars, halfStars] = getStarsCount(rating, maxRating, starsCount);
+function getStarsArray({fullStars, halfStars, starsCount}: StarRaterData): ReactElement[] {
 
     return Array.from({length: starsCount}, (_, index) => {
         return index < fullStars
-            ? StarElements.solidStar(index)
+            ? STARS.solidStar(index)
             : index < fullStars + halfStars
-                ? StarElements.halfStar(index)
-                : StarElements.emptyStar(index);
+                ? STARS.halfStar(index)
+                : STARS.emptyStar(index);
     });
     }
 
 
 const StarRater: FC<StarRaterProps> = ({rating, maxRating, starsCount = 5}) => {
+    const starRaterData = useMemo(() => getStarsCount(rating, maxRating, starsCount)
+            , [rating, maxRating, starsCount]);
     const title = `${rating} of ${maxRating}`;
     return (
         <HStack title={title}>
             <Text hideBelow={"xl"}>{title}</Text>
-            {getStarsArray(rating, maxRating, starsCount)}
+            {getStarsArray(starRaterData)}
         </HStack>
     );
 };
